f3f2b93ce0b4a74b197c52ed374f3c5cc203f269
joostlek <joostlek@outlook.com>
2018-07-07 11:10:30 +0200

    Stop the dart analyser falling over the test file not being updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,6 +4,7 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
+import 'package:eros/eros.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,7 +13,7 @@ import 'package:eros/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(new Eros());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
