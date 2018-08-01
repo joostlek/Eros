695adbabf3842bdc4dbe8d49458ecaeb1cef99e4
joostlek <joostlek@outlook.com>
2018-08-02 01:57:30 +0200

    Update graph layout to use tabs!

--- lib/pages/locations/stats/coupon_amount_chart.dart
+++ lib/pages/locations/stats/coupon_amount_chart.dart
@@ -18,206 +18,164 @@ class CouponAmountChart extends StatefulWidget {
   State<StatefulWidget> createState() => CouponAmountChartState();
 }
 
-class CouponAmountChartState extends State<CouponAmountChart> {
-  ValueNotifier<String> _selectedItem =
-      new ValueNotifier<String>(availableFilters[0]);
+class CouponAmountChartState extends State<CouponAmountChart>
+    with SingleTickerProviderStateMixin {
   CouponStorage couponStorage = CouponStorage();
-  static List<String> availableFilters = <String>[
-    'All',
-    'Giftcard',
-    'Discount',
-    'Item',
+  List<Coupon> coupons = <Coupon>[];
+
+  static List<CouponGraph> graphs = <CouponGraph>[
+    CouponGraph('All', []),
+    CouponGraph('Giftcard', []),
+    CouponGraph('Discount', []),
+    CouponGraph('Item', []),
   ];
 
-  _update(String string) {
-    setState(() {
-      _selectedItem.value = string;
+  TabController _tabController;
+
+  _getCoupons() {
+    couponStorage
+        .listCoupons(widget.location.locationId)
+        .listen((QuerySnapshot snapshot) {
+      coupons = snapshot.documents
+          .map((DocumentSnapshot document) =>
+              CouponStorage.fromDocument(document))
+          .toList();
+      coupons.sort((Coupon a, Coupon b) => a.issuedAt.millisecondsSinceEpoch
+          .compareTo(b.issuedAt.millisecondsSinceEpoch));
+      graphs[0].series = [
+        new charts.Series<Coupon, DateTime>(
+            id: 'amount',
+            colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+            displayName: 'amount',
+            domainFn: (Coupon coupon, int _) => coupon.issuedAt,
+            measureFn: (Coupon coupon, int _) {
+              int sum = 0;
+              coupons.getRange(0, _).forEach((Coupon) {
+                sum++;
+              });
+              return sum.floor();
+            },
+            data: coupons),
+      ];
+      graphs[1].series = [
+        new charts.Series<MoneyCoupon, DateTime>(
+            id: 'amount',
+            colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+            displayName: 'amount',
+            domainFn: (MoneyCoupon coupon, int _) => coupon.issuedAt,
+            measureFn: (MoneyCoupon coupon, int _) {
+              int sum = 0;
+              coupons.getRange(0, _).forEach((Coupon) {
+                sum++;
+              });
+              return sum.floor();
+            },
+            data: coupons
+                .where((Coupon coupon) => coupon.type == Coupons.MoneyCoupon)
+                .map((Coupon coupon) {
+              MoneyCoupon moneyCoupon = coupon;
+              return moneyCoupon;
+            }).toList()),
+      ];
+      graphs[2].series = [
+        new charts.Series<DiscountCoupon, DateTime>(
+            id: 'amount',
+            colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
+            displayName: 'amount',
+            domainFn: (DiscountCoupon coupon, int _) => coupon.issuedAt,
+            measureFn: (DiscountCoupon coupon, int _) {
+              int sum = 0;
+              coupons.getRange(0, _).forEach((Coupon) {
+                sum++;
+              });
+              return sum.floor();
+            },
+            data: coupons
+                .where((Coupon coupon) => coupon.type == Coupons.DiscountCoupon)
+                .map((Coupon coupon) {
+              DiscountCoupon discountCoupon = coupon;
+              return discountCoupon;
+            }).toList()),
+      ];
+      graphs[3].series = [
+        new charts.Series<ItemCoupon, DateTime>(
+            id: 'amount',
+            colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+            displayName: 'amount',
+            domainFn: (ItemCoupon coupon, int _) => coupon.issuedAt,
+            measureFn: (ItemCoupon coupon, int _) {
+              int sum = 0;
+              coupons.getRange(0, _).forEach((Coupon) {
+                sum++;
+              });
+              return sum.floor();
+            },
+            data: coupons
+                .where((Coupon coupon) => coupon.type == Coupons.ItemCoupon)
+                .map((Coupon coupon) {
+              ItemCoupon itemCoupon = coupon;
+              return itemCoupon;
+            }).toList()),
+      ];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCoupons();
+    _tabController = TabController(length: 4, vsync: this);
+    _tabController.addListener(() {
+      setState(() {});
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: couponStorage.listCoupons(widget.location.locationId),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasData && snapshot.data != null) {
-          List<Coupon> coupons = snapshot.data.documents
-              .map((DocumentSnapshot document) =>
-                  CouponStorage.fromDocument(document))
-              .toList();
-          coupons.sort((Coupon a, Coupon b) => a.issuedAt.millisecondsSinceEpoch
-              .compareTo(b.issuedAt.millisecondsSinceEpoch));
-          List<charts.Series<Coupon, DateTime>> series = [
-            new charts.Series<Coupon, DateTime>(
-                id: 'amount',
-                colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-                displayName: 'amount',
-                domainFn: (Coupon coupon, int _) => coupon.issuedAt,
-                measureFn: (Coupon coupon, int _) {
-                  int sum = 0;
-                  coupons.getRange(0, _).forEach((Coupon) {
-                    sum++;
-                  });
-                  return sum.floor();
-                },
-                data: coupons),
-          ];
-          List<charts.Series<Coupon, DateTime>> money_series = [
-            new charts.Series<MoneyCoupon, DateTime>(
-                id: 'amount',
-                colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-                displayName: 'amount',
-                domainFn: (MoneyCoupon coupon, int _) => coupon.issuedAt,
-                measureFn: (MoneyCoupon coupon, int _) {
-                  int sum = 0;
-                  coupons.getRange(0, _).forEach((Coupon) {
-                    sum++;
-                  });
-                  return sum.floor();
-                },
-                data: coupons
-                    .where(
-                        (Coupon coupon) => coupon.type == Coupons.MoneyCoupon)
-                    .map((Coupon coupon) {
-                  MoneyCoupon moneyCoupon = coupon;
-                  return moneyCoupon;
-                }).toList()),
-          ];
-          List<charts.Series<Coupon, DateTime>> discount_series = [
-            new charts.Series<DiscountCoupon, DateTime>(
-                id: 'amount',
-                colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-                displayName: 'amount',
-                domainFn: (DiscountCoupon coupon, int _) => coupon.issuedAt,
-                measureFn: (DiscountCoupon coupon, int _) {
-                  int sum = 0;
-                  coupons.getRange(0, _).forEach((Coupon) {
-                    sum++;
-                  });
-                  return sum.floor();
-                },
-                data: coupons
-                    .where((Coupon coupon) =>
-                        coupon.type == Coupons.DiscountCoupon)
-                    .map((Coupon coupon) {
-                  DiscountCoupon discountCoupon = coupon;
-                  return discountCoupon;
-                }).toList()),
-          ];
-          List<charts.Series<Coupon, DateTime>> item_series = [
-            new charts.Series<ItemCoupon, DateTime>(
-                id: 'amount',
-                colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-                displayName: 'amount',
-                domainFn: (ItemCoupon coupon, int _) => coupon.issuedAt,
-                measureFn: (ItemCoupon coupon, int _) {
-                  int sum = 0;
-                  coupons.getRange(0, _).forEach((Coupon) {
-                    sum++;
-                  });
-                  return sum.floor();
-                },
-                data: coupons
-                    .where((Coupon coupon) => coupon.type == Coupons.ItemCoupon)
-                    .map((Coupon coupon) {
-                  ItemCoupon itemCoupon = coupon;
-                  return itemCoupon;
-                }).toList()),
-          ];
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-                child: Column(
-              children: <Widget>[
-                ListTile(
-                    leading: Icon(Icons.local_activity),
-                    title: _selectedItem.value == 'Giftcard'
-                        ? Text('Amount of giftcards')
-                        : _selectedItem.value == 'Discount'
-                            ? Text('Amount of discount coupons')
-                            : _selectedItem.value == 'Item'
-                                ? Text('Amount of item coupons')
-                                : Text('Total amount of coupons'),
-                    subtitle: _selectedItem.value == 'Giftcard'
-                        ? Text('Total amount of giftcards added')
-                        : _selectedItem.value == 'Discount'
-                            ? Text('Total amount of discount coupons added')
-                            : _selectedItem.value == 'Item'
-                                ? Text('Total amount of item coupons added')
-                                : Text('Total amount of coupons'),
-                    trailing: PopupMenuButton<String>(
-                      icon: Icon(Icons.more_vert),
-                      onSelected: _update,
-                      itemBuilder: (BuildContext context) {
-                        return List<PopupMenuEntry<String>>.generate(
-                            availableFilters.length, (int index) {
-                          return PopupMenuItem(
-                              child: AnimatedBuilder(
-                                  child: Text(availableFilters[index]),
-                                  animation: _selectedItem,
-                                  builder:
-                                      (BuildContext context, Widget child) {
-                                    return RadioListTile<String>(
-                                      value: availableFilters[index],
-                                      groupValue: _selectedItem.value,
-                                      title: child,
-                                      onChanged: (String filter) {
-                                        _update(filter);
-                                        Navigator.pop(context);
-                                      },
-                                    );
-                                  }));
-                        });
-                      },
-                    )),
-                Container(
-                    height: 150.0,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 8.0),
-                      child: _selectedItem.value == 'Giftcard' &&
-                              money_series[0].data.length > 1
-                          ? charts.TimeSeriesChart(
-                              money_series,
-                              defaultRenderer: charts.LineRendererConfig(
-                                  includeArea: true, stacked: true),
-                            )
-                          : _selectedItem.value == 'Discount' &&
-                                  discount_series[0].data.length > 1
-                              ? charts.TimeSeriesChart(
-                                  discount_series,
-                                  defaultRenderer: charts.LineRendererConfig(
-                                      includeArea: true, stacked: true),
-                                )
-                              : _selectedItem.value == 'Item' &&
-                                      item_series[0].data.length > 1
-                                  ? charts.TimeSeriesChart(
-                                      item_series,
-                                      defaultRenderer:
-                                          charts.LineRendererConfig(
-                                              includeArea: true, stacked: true),
-                                    )
-                                  : _selectedItem.value == 'All' &&
-                                          series[0].data.length > 1
-                                      ? charts.TimeSeriesChart(
-                                          series,
-                                          defaultRenderer:
-                                              charts.LineRendererConfig(
-                                                  includeArea: true,
-                                                  stacked: true),
-                                        )
-                                      : Center(
-                                          child: Text('Not enough data'),
-                                        ),
-                    )),
-              ],
-            )),
-          );
-        } else {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
-    );
+    return Card(
+        child: Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.local_activity),
+          title: Text('Total amount of coupons'),
+          subtitle: Text('Total amount of coupons'),
+        ),
+        TabBar(
+          controller: _tabController,
+          labelColor: Colors.black,
+          tabs: <Widget>[
+            Tab(
+              text: 'All',
+            ),
+            Tab(
+              text: 'Giftcard',
+            ),
+            Tab(
+              text: 'Discount',
+            ),
+            Tab(
+              text: 'Item',
+            )
+          ],
+        ),
+        Container(
+          height: 150.0,
+          child: Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 8.0),
+              child: graphs[_tabController.index].series[0].data.length > 1
+                  ? charts.TimeSeriesChart(
+                      graphs[_tabController.index].series,
+                    )
+                  : Center(child: Text('Not enough data'))),
+        )
+      ],
+    ));
   }
+}
+
+class CouponGraph {
+  final String name;
+  List<charts.Series<Coupon, DateTime>> series;
+
+  CouponGraph(this.name, this.series);
 }
